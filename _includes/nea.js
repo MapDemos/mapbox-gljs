b50b4cbc34094f4287e1b4192541ed3e18ee3d8e
kenji-shima <kenji.shima@mapbox.com>
2024-11-28 17:13:54 +0900

    nea and walay

---
+++ _includes/nea.js
@@ -0,0 +1,105 @@
+
+lat = 1.4214734600763848
+lng = 103.87371902395148
+const tilesets = {
+    n1d_z4: {
+        value: "mapbox://kenji-shima.nea-N1D-z4",
+        label: "N1D Zoom 4",
+        type: 'raster-array'
+    },
+    n1d_z8: {
+        value: "mapbox://kenji-shima.nea-N1D-z8",
+        label: "N1D Zoom 8",
+        type: 'raster-array'
+    },
+    n1d_z12: {
+        value: "mapbox://kenji-shima.nea-N1D-z12",
+        label: "N1D Zoom 12",
+        type: 'raster-array'
+    },
+    gfs_z4: {
+        value: "mapbox://kenji-shima.nea-gfs-z4",
+        label: "GFS Zoom 4",
+        type: 'raster-array'
+    },
+    gfs_z8: {
+        value: "mapbox://kenji-shima.nea-gfs-z8",
+        label: "GFS Zoom 8",
+        type: 'raster-array'
+    },
+    radar_z4: {
+        value: "mapbox://kenji-shima.nea-radar",
+        label: "Radar Zoom 4",
+        type: 'raster-array',
+        zoom: 11.5
+    },
+    radar_z8: {
+        value: "mapbox://kenji-shima.nea-radar-8",
+        label: "Radar Zoom 8",
+        type: 'raster-array',
+        zoom: 11.5
+    },
+    radar_z12: {
+        value: "mapbox://kenji-shima.nea-radar-12",
+        label: "Radar Zoom 12",
+        type: 'raster-array',
+        zoom: 11.5
+    },
+    radar_z16: {
+        value: "mapbox://kenji-shima.nea-radar-test",
+        label: "Radar Zoom 16",
+        type: 'raster-array',
+        zoom: 11.5
+    },
+    satellite_H8_ASEAN_z8: {
+        value: "mapbox://kenji-shima.nea-satellite-h8-asean-z8",
+        label: "Satellite H8 ASEAN Zoom 8",
+        type: 'raster'
+    },
+    satellite_H8_ASEAN_z10: {
+        value: "mapbox://kenji-shima.nea-satellite-h8-asean-z10",
+        label: "Satellite H8 ASEAN Zoom 10",
+        type: 'raster'
+    },
+    satellite_H8_ASEAN_z12: {
+        value: "mapbox://kenji-shima.nea-satellite-h8-asean-z12",
+        label: "Satellite H8 ASEAN Zoom 12",
+        type: 'raster'
+    },
+    satellite_smoke_z4: {
+        value: "mapbox://kenji-shima.nea-satellite-smoke",
+        label: "Satellite Smoke Zoom 4",
+        type: 'raster'
+    },
+    satellite_smoke_z8: {
+        value: "mapbox://kenji-shima.nea-satellite-smoke-z8",
+        label: "Satellite Smoke Zoom 8",
+        type: 'raster'
+    },
+    satellite_smoke_z10: {
+        value: "mapbox://kenji-shima.nea-satellite-smoke-z10",
+        label: "Satellite Smoke Zoom 10",
+        type: 'raster'
+    },
+    satellite_smoke_z12: {
+        value: "mapbox://kenji-shima.nea-satellite-smoke-z12",
+        label: "Satellite Smoke Zoom 12",
+        type: 'raster'
+    },
+    lightning_z16: {
+        value: "mapbox://kenji-shima.nied-lightning",
+        label: "Lightning Zoom 4-16",
+        type: 'vector',
+        vector_layer_type: 'circle',
+        vector_layer_source: 'lightning',
+        zoom: 4
+    },
+    hotspot_z16: {
+        value: "mapbox://kenji-shima.nied-hotspot",
+        label: "Hotspot Zoom 4-16",
+        type: 'vector',
+        vector_layer_type: 'circle',
+        vector_layer_source: 'hotspot',
+        zoom: 4
+    },
+}
---
+++ _includes/weatherwalay.js
@@ -0,0 +1,35 @@
+
+lat = 33.038945863053385
+lng = 72.49649380770748
+const tilesets = {
+    walay_z4: {
+        value: "mapbox://kenji-shima.walay-radar-202311092301-z4",
+        label: "Zoom 4",
+        type: 'raster-array',
+        zoom: 7
+    },
+    walay_z6: {
+        value: "mapbox://kenji-shima.walay-z6",
+        label: "Zoom 6",
+        type: 'raster-array',
+        zoom: 7
+    },
+    walay_z8: {
+        value: "mapbox://kenji-shima.walay-z8",
+        label: "Zoom 8",
+        type: 'raster-array',
+        zoom: 7
+    },
+    walay_z12: {
+        value: "mapbox://kenji-shima.walay-z12",
+        label: "Zoom 12",
+        type: 'raster-array',
+        zoom: 7
+    },
+    walay_z16: {
+        value: "mapbox://kenji-shima.walay-z16",
+        label: "Zoom 16",
+        type: 'raster-array',
+        zoom: 7
+    }
+}
