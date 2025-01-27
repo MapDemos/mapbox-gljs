31db139a455ed61244b9e992180ff7f28d2f1e3b
kenji-shima <kenji.shima@mapbox.com>
2025-01-27 13:20:49 +0900

    raster-compare追加

---
+++ _includes/raster-compare.js
@@ -0,0 +1,107 @@
+
+lat = 35.67881527736655
+lng = 139.76652995236685
+const tilesets = {
+    nowcast30_z12: {
+        value: "mapbox://kenji-shima.nowcast-20240620195000-30m-z12",
+        label: "Nowcast 30 min Zoom 12",
+        type: 'raster-array',
+        zoom: 7
+    },
+    nowcast30_z9: {
+        value: "mapbox://kenji-shima.nowcast-20240620195000-30m",
+        label: "Nowcast 30 min Zoom 9",
+        type: 'raster-array',
+        zoom: 7
+    },
+    nowcast30_z8: {
+        value: "mapbox://kenji-shima.nowcast-20240620195000-30m-zoom8",
+        label: "Nowcast 30 min Zoom 8",
+        type: 'raster-array',
+        zoom: 7
+    },
+    nowcast30_z6: {
+        value: "mapbox://kenji-shima.nowcast-20240620195000-30m-zoom6",
+        label: "Nowcast 30 min Zoom 6",
+        type: 'raster-array',
+        zoom: 7
+    },
+    nowcast30_z4: {
+        value: "mapbox://kenji-shima.nowcast-20240620195000-30m-zoom4",
+        label: "Nowcast 30 min Zoom 4",
+        type: 'raster-array',
+        zoom: 7
+    },
+    nowcast60_z12: {
+        value: "mapbox://kenji-shima.nowcast-20240620195000-60m-z12",
+        label: "Nowcast 60 min Zoom 12",
+        type: 'raster-array',
+        zoom: 7
+    },
+    nowcast60_z9: {
+        value: "mapbox://kenji-shima.nowcast-20240620195000-60m",
+        label: "Nowcast 60 min Zoom 9",
+        type: 'raster-array',
+        zoom: 7
+    },
+    nowcast60_z8: {
+        value: "mapbox://kenji-shima.nowcast-20240620195000-60m-zoom8",
+        label: "Nowcast 60 min Zoom 8",
+        type: 'raster-array',
+        zoom: 7
+    },
+    nowcast60_z6: {
+        value: "mapbox://kenji-shima.nowcast-20240620195000-60m-zoom6",
+        label: "Nowcast 60 min Zoom 6",
+        type: 'raster-array',
+        zoom: 7
+    },
+    nowcast60_z4: {
+        value: "mapbox://kenji-shima.nowcast-20240620195000-60m-zoom4",
+        label: "Nowcast 60 min Zoom 4",
+        type: 'raster-array',
+        zoom: 7
+    },
+    forecast6_z9: {
+        value: "mapbox://kenji-shima.forecast-20240620203000-6h",
+        label: "Forecast 6 hours Zoom 9",
+        type: 'raster-array',
+        zoom: 7
+    },
+    forecast6_z8: {
+        value: "mapbox://kenji-shima.forecast-20240620203000-6h-z8",
+        label: "Forecast 6 hours Zoom 8",
+        type: 'raster-array',
+        zoom: 7
+    },
+    forecast6_z6: {
+        value: "mapbox://kenji-shima.forecast-20240620203000-6h-z6",
+        label: "Forecast 6 hours Zoom 6",
+        type: 'raster-array',
+        zoom: 7
+    },
+    forecast15_z9: {
+        value: "mapbox://kenji-shima.forecast-20240620200000-15h",
+        label: "Forecast 15 hours Zoom 9",
+        type: 'raster-array',
+        zoom: 7
+    },
+    forecast15_z8: {
+        value: "mapbox://kenji-shima.forecast-20240620200000-15h-z8",
+        label: "Forecast 15 hours Zoom 8",
+        type: 'raster-array',
+        zoom: 7
+    },
+    forecast15_z6: {
+        value: "mapbox://kenji-shima.forecast-20240620200000-15h-z6",
+        label: "Forecast 15 hours Zoom 6",
+        type: 'raster-array',
+        zoom: 7
+    },
+    forecast15_z4: {
+        value: "mapbox://kenji-shima.forecast-20240620200000-15h-z4",
+        label: "Forecast 15 hours Zoom 4",
+        type: 'raster-array',
+        zoom: 7
+    },
+}
