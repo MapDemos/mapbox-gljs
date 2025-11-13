b3c266b7e71a1b06614303402f745ef0133da191
kenji-shima <kenji.shima@mapbox.com>
2025-11-13 12:42:06 +0900

    added winds

---
+++ _includes/wind.js
@@ -0,0 +1,33 @@
+lat = -24.635070733310073;
+lng = 134.6455347407337;
+
+const tilesets = {
+    wind_4: {
+        value: "mapbox://kenji-shima.bom-wind-z4",
+        label: "wind zoom 4",
+        type: 'raster-array-particle',
+        mapstyle: 'mapbox://styles/mapbox/satellite-v9',
+        maplayer_above: 'satellite',
+    },
+    wind_6: {
+        value: "mapbox://kenji-shima.bom-wind-z6",
+        label: "wind zoom 6",
+        type: 'raster-array-particle',
+        mapstyle: 'mapbox://styles/mapbox/satellite-v9',
+        maplayer_above: 'satellite',
+    },
+    wind_8: {
+        value: "mapbox://kenji-shima.bom-wind-z8",
+        label: "wind zoom 8",
+        type: 'raster-array-particle',
+        mapstyle: 'mapbox://styles/mapbox/satellite-v9',
+        maplayer_above: 'satellite',
+    },
+    wind_10: {
+        value: "mapbox://kenji-shima.bom-wind-z10",
+        label: "wind zoom 10",
+        type: 'raster-array-particle',
+        mapstyle: 'mapbox://styles/mapbox/satellite-v9',
+        maplayer_above: 'satellite',
+    },
+}
